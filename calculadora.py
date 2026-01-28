4b7ce7c0f03248f595dad409cf27a7ded1cf0c2e
= <=>
2026-01-28 10:07:24 -0300

    Implementando a operação de subtrair

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,11 @@
+# Titulo da calculadora
 print("Calculadora em python")
+
+# Obtendo os numeros e operação
+num1 = float(input("digite o primeiro numero:"))
+#operador = input("digite o operador (+):")
+num2 = float(input("digite o segundo numero:"))
+
+resultado = num1 - num2
+
+print("resultado:", resultado)
